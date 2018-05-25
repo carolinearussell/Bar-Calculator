e1c093956a12a11c33817f352059a0b60bfcc1f5
Caroline Russell <carolinerussell2017@gmail.com>
2018-05-24 22:27:08 -0400

    changes to files

--- main.js
+++ main.js
@@ -1,53 +1,61 @@
-//Calculate Tip
-function calculateTip() {
-  let billAmt = document.getElementById("billAmt").value;
-  let tipAmt= document.getElementById("tipAmt").value;
-  let peopleAmt = document.getElementById("peopleAmt").value;
+  
 
-  //validate input
-  if (billAmt === "" || tipAmt == 0) {
-    alert("Please enter values");
-    return;
-  }
-  //Check to see if this input is empty or less than or equal to 1
-  if (numOfPeople === "" || numOfPeople <= 1) {
-    numOfPeople = 1;
-    document.getElementById("each").style.display = "none";
-  } else {
-    document.getElementById("each").style.display = "block";
-  }
+  
+// function calculate numbers enter from the screen to produce tip information
+document.getElementById('calculate').onclick = function calculateTip(){
+  var billAmt = Number(document.getElementById("billAmt").value);
+  var tipPer1 = Number(document.getElementById("tipPer").value);
+  var peopleAmt = Number(document.getElementById("peopleAmt").value);
+  var drinkCost = Number(document.getElementById("drinkCost").value);
+  var drinkRd = Number(document.getElementById("drinkRd").value);
+  var bill = billAmt;
+// calculation for tip
+    tipPer = tipPer1/100;
+    billBeforeTip = billAmt/peopleAmt;
+    tipAmt = bill * tipPer;
+    totalBill = bill + tipAmt;
+    billPPAmt = bill/peopleAmt;
+    tipAmtPPerson = tipAmt/peopleAmt;
+    totalbbTip = (drinkCost * drinkRd)/2
+    splitBuyBack = totalbbTip/peopleAmt;
+    totalBillAmt = billPPAmt + tipAmtPPerson + totalbbTip;
+    totalbillTP = (totalBill + totalbbTip)/peopleAmt;
 
-  //Calculate tip
-  let total = (billAmt * [tipAmt/100]) / peopleAmt;
-  //round to two decimal places
-  total = Math.round(total * 100) / 100;
-  //next line allows us to always have two digits after decimal point
-  total = total.toFixed(2);
-  //Display the tip
-  document.getElementById("totalTip").style.display = "block";
-  document.getElementById("tip").innerHTML = total;
-
-
-
-//click to call function
-document.getElementById("clear").onclick = function() {
-  calculateTip();
-
-};
-
-// calculate buyback
-let buyback = (drinkCost * drinkRd)* 0.50
-// round to decimal places
-total = Math.round(buyback * 100) / 100;
-  //next line allows us to always have two digits after decimal point
-  total = total.toFixed(2);
-// //Display the tip
-  document.getElementById("tt.ppp").style.display = "block";
-  document.getElementById("tip").innerHTML = total;
+// display tip inform to the screen
+  document.getElementById("tBill").innerHTML= "$"+Number(totalBill).toFixed(2);
+      console.log(Number(totalBill).toFixed(2));
+   document.getElementById("tAmt").innerHTML= "$"+Number(tipAmt).toFixed(2);
+      console.log(Number(tipAmt).toFixed(2));
+  document.getElementById("bPP").innerHTML= "$"+Number(billPPAmt).toFixed(2);
+      console.log(Number(billPPAmt).toFixed(2));
+    document.getElementById("bbPP").innerHTML= "$"+Number(totalbillTP).toFixed(2);
+      console.log(Number(totalbillTP).toFixed(2));   
+  document.getElementById("tPP").innerHTML= "$"+Number(tipAmtPPerson).toFixed(2);
+      console.log(Number(tipAmtPPerson).toFixed(2));  
+  document.getElementById("bbC").innerHTML= "$"+Number(totalbbTip).toFixed(2);
+      console.log(Number(totalbbTip).toFixed(2));  
+  document.getElementById("splitBBack").innerHTML= "$"+Number(splitBuyBack).toFixed(2);
+      console.log(Number(splitBuyBack).toFixed(2));
+}
 
 
-//Hide the tip amount on load
-document.getElementById("totalTip").style.display = "none";
-document.getElementById("each").style.display = "none";
+
+// Clear screen inputs
+  document.getElementById('clear').onclick = function clear() {
+    var elementsToClear = document.getElementsByClassName("clear-me");
+    var inputsToClear  = document.getElementsByClassName("clear-value");
+
+    // console.log(elementsToClear);
+    // console.log(inputsToClear);
+
+    for (var i = 0; i < elementsToClear.length; i++){
+      elementsToClear[i].innerHTML = '';  
+    }
+    for (var k = 0; k < inputsToClear.length; k++){
+      inputsToClear[k].value = '';
+    }
+
+}
+
 
 
