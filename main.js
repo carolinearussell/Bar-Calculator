f7b5d4442a42968c1727a5211921d8c0032effad
David Ashe <david.ashe@aexp.com>
2018-08-09 08:34:49 -0400

    more cleanup

--- main.js
+++ main.js
@@ -4,41 +4,32 @@ const handleCalculate = () => {
   const peopleAmt = document.getElementById("peopleAmt");
   const drinkCost = document.getElementById("drinkCost");
   const drinkRd = document.getElementById("drinkRd");
-// calculation for tip
-    tipPer = tipPer1/100;
-    billBeforeTip = billAmt/peopleAmt;
-    tipAmt = bill * tipPer;
-    totalBill = bill + tipAmt;
-    billPPAmt = bill/peopleAmt;
-    tipAmtPPerson = tipAmt/peopleAmt;
-    totalbbTip = (drinkCost * drinkRd)/2
-    splitBuyBack = totalbbTip/peopleAmt;
-    totalBillAmt = billPPAmt + tipAmtPPerson + totalbbTip;
-    totalbillTP = (totalBill + totalbbTip)/peopleAmt;
 
-// display tip inform to the screen
-  document.getElementById("tBill").innerHTML= "$"+Number(totalBill).toFixed(2);
-      console.log(Number(totalBill).toFixed(2));
-   document.getElementById("tAmt").innerHTML= "$"+Number(tipAmt).toFixed(2);
-      console.log(Number(tipAmt).toFixed(2));
-  document.getElementById("bPP").innerHTML= "$"+Number(billPPAmt).toFixed(2);
-      console.log(Number(billPPAmt).toFixed(2));
-    document.getElementById("bbPP").innerHTML= "$"+Number(totalbillTP).toFixed(2);
-      console.log(Number(totalbillTP).toFixed(2));   
-  document.getElementById("tPP").innerHTML= "$"+Number(tipAmtPPerson).toFixed(2);
-      console.log(Number(tipAmtPPerson).toFixed(2));  
-  document.getElementById("bbC").innerHTML= "$"+Number(totalbbTip).toFixed(2);
-      console.log(Number(totalbbTip).toFixed(2));  
-  document.getElementById("splitBBack").innerHTML= "$"+Number(splitBuyBack).toFixed(2);
-      console.log(Number(splitBuyBack).toFixed(2));
+  // calculation for tip
+  let tipPer = tipPer1/100;
+  let billBeforeTip = billAmt/peopleAmt;
+  let tipAmt = bill * tipPer;
+  let totalBill = bill + tipAmt;
+  let billPPAmt = bill/peopleAmt;
+  let tipAmtPPerson = tipAmt/peopleAmt;
+  let totalbbTip = (drinkCost * drinkRd)/2
+  let splitBuyBack = totalbbTip/peopleAmt;
+  let totalBillAmt = billPPAmt + tipAmtPPerson + totalbbTip;
+  let totalbillTP = (totalBill + totalbbTip)/peopleAmt;
+
+  // display tip inform to the screen
+  document.getElementById("tBill").innerHTML= "$" + Number(totalBill).toFixed(2);
+  document.getElementById("tAmt").innerHTML= "$" + Number(tipAmt).toFixed(2);
+  document.getElementById("bPP").innerHTML= "$" + Number(billPPAmt).toFixed(2);
+  document.getElementById("bbPP").innerHTML= "$" + Number(totalbillTP).toFixed(2); 
+  document.getElementById("tPP").innerHTML= "$" + Number(tipAmtPPerson).toFixed(2);
+  document.getElementById("bbC").innerHTML= "$" + Number(totalbbTip).toFixed(2);
+  document.getElementById("splitBBack").innerHTML= "$" + Number(splitBuyBack).toFixed(2);
 };
 
 const clear = () => {
   var elementsToClear = document.getElementsByClassName("clear-me");
   var inputsToClear  = document.getElementsByClassName("clear-value");
-
-  // console.log(elementsToClear);
-  // console.log(inputsToClear);
 
   for (var i = 0; i < elementsToClear.length; i++){
     elementsToClear[i].innerHTML = '';  
